b100de08f6c037600156eebb64f8206e31450694
NOUALI93 <61235890+NOUALI93@users.noreply.github.com>
2025-10-18 01:56:56 +0200

    LWC accountOpportunitiesViewer correction

--- force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
+++ force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
@@ -1,4 +1,6 @@
 import { LightningElement, api, wire, track } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getOpportunities from '@salesforce/apex/AccountOpportunitiesController.getOpportunities';
 
 export default class AccountOpportunitiesViewer extends LightningElement {
@@ -6,6 +8,7 @@ export default class AccountOpportunitiesViewer extends LightningElement {
     @track opportunities;
     @track error;
     @track nodata;
+    @track wiredOpportunitiesResult;
     columns = [
         { label: 'Nom Opportunité', fieldName: 'Name', type: 'text' },
         { label: 'Montant', fieldName: 'Amount', type: 'currency' },
@@ -13,17 +16,33 @@ export default class AccountOpportunitiesViewer extends LightningElement {
         { label: 'Phase', fieldName: 'StageName', type: 'text' }
     ];
 
-    @wire(getOpportunities, { accountId: '$recordId' }) //error
-    wiredOpportunities({ error, data }) {
-        if (data=[]) {
-            this.nodata = data;
-        } else if (data){
+    @wire(getOpportunities, { accountId: '$recordId' }) 
+    wiredOpportunities(result) {
+        this.wiredOpportunitiesResult = result;
+        const { error , data } = result;
+        this.nodata = false;
+         if (data?.length){
             this.opportunities = data;
         } else if (error) {
             this.error = error;
             this.opportunities = undefined;
+        } else {
+            this.nodata = true;
         }
     }
 
+    async handleRafraichir() {
+        try{
+            await refreshApex(this.wiredOpportunitiesResult);
+            this.displayToast('Succès', 'Les données ont été actualisées !', 'success');
+        }
+        catch (error){
+            this.displayToast('Erreur', 'Impossible de rafraîchir les données.', 'error');
+        }
+    }
 
+    displayToast(title, message, variant){
+        const eventToast = new ShowToastEvent({title, message, variant});
+        this.dispatchEvent(eventToast);
+    }
 }
