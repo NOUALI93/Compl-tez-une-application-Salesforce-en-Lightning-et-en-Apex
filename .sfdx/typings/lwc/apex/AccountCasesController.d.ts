ff477ea761a262dc206feeb483babaa75e10d41f
NOUALI93 <61235890+NOUALI93@users.noreply.github.com>
2025-10-30 17:39:27 +0100

    implementing Tests

--- .sfdx/typings/lwc/apex/AccountCasesController.d.ts
+++ .sfdx/typings/lwc/apex/AccountCasesController.d.ts
@@ -1,3 +1,3 @@
 declare module "@salesforce/apex/AccountCasesController.findCasesBySubject" {
-  export default function findCasesBySubject(): Promise<any>;
+  export default function findCasesBySubject(param: {accountId: any, subjectSearchTerm: any}): Promise<any>;
 }
